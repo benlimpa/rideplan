bc915dbf115d89f055dae1d5525418e27fa6b2cd
Kimeiga <hak7alp@gmail.com>
2018-02-17 20:38:07 -0800

    profile card image is fixed

--- src/app/components/map/planCard.js
+++ src/app/components/map/planCard.js
@@ -13,7 +13,7 @@ export default class PlanCard extends React.Component
       <div className={styles["planCard"]}>
         <div className={styles["fir-image-figure"]}>
           <div className={styles["left-20"]}>
-            <img className={styles["fir-author-image fir-clickcircle"]}
+            <img className={styles["fir-clickcircle"]}
                  src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/26992356_574807149527770_1756966645001432152_n.jpg?oh=4ff13172d22daf8ba7538d80fe5057d8&oe=5B237390"></img>
               <button className={styles["button"]}> JOIN </button>
           </div>
